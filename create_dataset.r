63fc8921c6e7fc4bb65eaad9d9056bc2899867c1
Yuriy D. Sibirmovsky <76599593+YDS67@users.noreply.github.com>
2023-09-24 21:51:12 +0300

    Simplified some stuff

--- create_dataset.r
+++ create_dataset.r
@@ -1,15 +1,16 @@
-filename <- "dataset_smooth.dat"
+fermi <- function(x,temp){1/(1+exp(x/temp))}
+
+filename <- "dataset_fermi.dat"
 
 n_points <- 100
 
-fermi <- function(x,temp){1/(1+exp(x/temp))}
-
 x <- (-n_points):(n_points) / n_points
 
-y1 <- fermi(x,0.05)
+y1 <- c(rep(1, n_points), 0.5, rep(0, n_points))
 y2 <- fermi(x,0.1)
-y3 <- fermi(x,0.5)
+y3 <- fermi(x,0.2)
 
 data <- data.frame(x, y1, y2, y3)
 
 write.table(data, filename, row.names = FALSE, col.names = FALSE)
+
--- plot_base_color.r
+++ plot_base_color.r
@@ -1,6 +1,6 @@
 #Color plot of smooth lines with R base package
 
-data <- read.table("dataset_smooth.dat")
+data <- read.table("dataset_fermi.dat")
 
 x <- data$V1
 y1 <- data$V2
@@ -15,46 +15,53 @@ y_max <- max(y1,y2,y3)
 y_min <- min(y1,y2,y3)
 y_height <- y_max-y_min
 
-png("plot_smooth_1.png", width = 1200, height = 1000, units = "px", pointsize = 30)
+cols <- c("black", "blue", "red")
+ltys <- c(2, 1, 5)
+
+filename <- "plot_fermi_1"
+
+png(paste(filename, ".png", sep = ""), width = 1200, height = 1000, units = "px", pointsize = 30)
 par(mar = c(5.1, 5.1, 2.1, 2.1), lwd = 3)
 line_width <- 5
 
 plot(x, y1, type = "l", ylim = c(y_min, y_max), 
     lwd = line_width, 
-    col = "blue", 
+    col = cols[1],
+    lty = ltys[1], 
     xlab = "Энергия, усл. ед.", 
     ylab = "Распределение Ферми", 
     cex.lab = 1.5)
-lines(x, y2, lwd = line_width, col = "red", lty = 2)
-lines(x, y3, lwd = line_width, col = "purple", lty = 4)
+lines(x, y2, lwd = line_width, col = cols[2], lty = ltys[2])
+lines(x, y3, lwd = line_width, col = cols[3], lty = ltys[3])
 
 legend(x_min + x_length*0.6, y_max - y_height*0.05, 
-    legend = c("kT = 0.05", "kT = 0.1", "kT = 0.5"), 
+    legend = c("kT = 0", "kT = 0.1", "kT = 0.2"), 
     lwd = rep(line_width, 3), 
-    lty = c(1,2,4), 
-    col = c("blue", "red", "purple"), 
+    lty = ltys, 
+    col = cols, 
     cex = 1.2)
 
 dev.off()
 
-pdf("plot_smooth_1.pdf", width = 6, height = 5)
+pdf(paste(filename, ".pdf", sep = ""), width = 6, height = 5)
 par(mar = c(5.1, 5.1, 2.1, 2.1), lwd = 1)
 line_width <- 2
 
 plot(x, y1, type = "l", ylim = c(y_min, y_max), 
     lwd = line_width, 
-    col = "blue", 
+    col = cols[1],
+    lty = ltys[1], 
     xlab = "Energy, a.u.", 
     ylab = "Fermi distribution", 
     cex.lab = 1.2)
-lines(x, y2, lwd = line_width, col = "red", lty = 2)
-lines(x, y3, lwd = line_width, col = "purple", lty = 4)
+lines(x, y2, lwd = line_width, col = cols[2], lty = ltys[2])
+lines(x, y3, lwd = line_width, col = cols[3], lty = ltys[3])
 
 legend(x_min + x_length*0.6, y_max - y_height*0.05, 
-    legend = c("kT = 0.05", "kT = 0.1", "kT = 0.5"), 
+    legend = c("kT = 0", "kT = 0.1", "kT = 0.2"), 
     lwd = rep(line_width, 3), 
-    lty = c(1,2,4), 
-    col = c("blue", "red", "purple"), 
-    cex = 1)
+    lty = ltys, 
+    col = cols, 
+    cex = 1.0)
 
 dev.off()
