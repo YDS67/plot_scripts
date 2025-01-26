3fc26a25fe8ad24a8a2593c0fbde038608677b3d
Yuriy D. Sibirmovsky <76599593+YDS67@users.noreply.github.com>
2025-01-26 22:57:15 +0300

    Add more colors

--- python-plotly/taylor.py
+++ python-plotly/taylor.py
@@ -33,6 +33,19 @@ c = [
     "rgb(136, 34, 85)",   # wine
 ]
 
+c1 = [
+    "#377EB8", # blue
+    "#FF7F00", # orange
+    "#4DAF4A", # green
+    "#F781BF", # pink
+    "#A65628", # brown
+    "#984EA3", # purple
+    "#FFFF33", # yellow
+    "#E41A1C", # red
+    "#999999", # grey
+    "#A6CEE3", # cyan
+]
+
 d = [
     'solid',
     'dot',
@@ -44,11 +57,11 @@ d = [
 
 # Create traces
 lwd = 3
-trace1 = go.Scatter(x=x, y=y1, mode='lines', name=mr.html("$\\cos x^2$"), line=dict(color=c[0], dash=d[0], width=lwd))
-trace2 = go.Scatter(x=x, y=y2, mode='lines', name=mr.html("$T_1(x)$"), line=dict(color=c[1], dash=d[1], width=lwd))
-trace3 = go.Scatter(x=x, y=y3, mode='lines', name=mr.html("$T_2(x)$"), line=dict(color=c[2], dash=d[2], width=lwd))
-trace4 = go.Scatter(x=x, y=y4, mode='lines', name=mr.html("$T_3(x)$"), line=dict(color=c[3], dash=d[3], width=lwd))
-trace5 = go.Scatter(x=x, y=y5, mode='lines', name=mr.html("$T_4(x)$"), line=dict(color=c[4], dash=d[4], width=lwd))
+trace1 = go.Scatter(x=x, y=y1, mode='lines', name=mr.html("$\\cos x^2$"), line=dict(color=c1[0], dash=d[0], width=lwd))
+trace2 = go.Scatter(x=x, y=y2, mode='lines', name=mr.html("$T_1(x)$"), line=dict(color=c1[1], dash=d[1], width=lwd))
+trace3 = go.Scatter(x=x, y=y3, mode='lines', name=mr.html("$T_2(x)$"), line=dict(color=c1[2], dash=d[2], width=lwd))
+trace4 = go.Scatter(x=x, y=y4, mode='lines', name=mr.html("$T_3(x)$"), line=dict(color=c1[3], dash=d[3], width=lwd))
+trace5 = go.Scatter(x=x, y=y5, mode='lines', name=mr.html("$T_4(x)$"), line=dict(color=c1[4], dash=d[4], width=lwd))
 
 xaxis = dict(
     title=dict(text=mr.html("$x$"),font=dict(family='Times', size=25, color="black")),
